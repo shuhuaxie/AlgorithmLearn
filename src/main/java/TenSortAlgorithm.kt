c96a7fcc288c444c00eafe62c0388d7d572561c4
xie <xieshuhua@163.com>
2018-12-12 08:24:12 +0800

    add

--- src/main/java/TenSortAlgorithm.kt
+++ src/main/java/TenSortAlgorithm.kt
@@ -1,9 +1,18 @@
 object TenSortAlgorithm {
-    var testArr = intArrayOf(3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48)
+    /**
+     *  参考自 @link https://www.cnblogs.com/onepixel/articles/7674659.html
+     *  十大经典排序算法（动图演示）
+     */
 
+    var testArr = intArrayOf(
+            3, 44, 38, 5, 47,
+            15, 36, 26, 27, 2,
+            46, 4, 19, 50, 48)
+
+    // 冒泡排序 从最小的开始排
     fun bubbleSort1(arr: IntArray): IntArray {
-        for (i in 0 until arr.size - 1) {
-            for (j in i + 1 until arr.size - 1) {
+        for (i in 0 until arr.size) {
+            for (j in i + 1 until arr.size) {
                 if (arr[i] > arr[j]) {
                     val temp = arr[i]
                     arr[i] = arr[j]
@@ -14,14 +23,81 @@ object TenSortAlgorithm {
         return arr
     }
 
+    // 冒泡排序 从最大的开始排
+    fun bubbleSort2(arr: IntArray): IntArray {
+        for (i in 0 until arr.size) {
+            for (j in 0 until arr.size - i) {
+                if (arr[j] > arr[j + 1]) {
+                    val temp = arr[j]
+                    arr[j] = arr[j + 1]
+                    arr[j + 1] = temp
+                }
+            }
+        }
+        return arr
+    }
+
+    // 选择排序
+    fun selectionSort(arr: IntArray): IntArray {
+        for (i in 0 until arr.size - 1) {
+            var tempSmall = i
+            for (j in i + 1 until arr.size) {
+                if (arr[j] < arr[tempSmall]) {
+                    tempSmall = j
+                }
+            }
+            val temp = arr[tempSmall]
+            arr[tempSmall] = arr[i]
+            arr[i] = temp
+        }
+        return arr
+    }
+
+    // 插入排序
+    fun insertionSort(arr: IntArray): IntArray {
+        for (i in 1 until arr.size) {
+            var startIndex = i - 1
+            var tempValue = arr[i]
+            while (startIndex >= 0 && arr[startIndex] > tempValue) {
+                arr[startIndex + 1] = arr[startIndex]
+                startIndex--
+            }
+            arr[startIndex + 1] = tempValue
+
+        }
+        return arr
+    }
+
+
+    // 希尔排序
+    fun shellSort(arr: IntArray): IntArray {
+        var len = 1
+        while (len < arr.size / 3) {
+            len = len * 3 + 1
+        }
+        while (len > 0) {
+            for (i in len until arr.size) {
+                var temp = arr[i]
+                var j = i - len
+                while (j > -1 && arr[j] > temp) {
+                    arr[j + len] = arr[j]
+                    j -= len
+                }
+                arr[j + len] = temp
+
+            }
+            len = Math.floorDiv(len,3)
+        }
+        return arr
+    }
 }
 
 fun IntArray.print() {
-    for (i in 0 until size - 1) {
-        print(if (i != 0) {
-            ","
+    for (i in 0 until size) {
+        if (i != size - 1) {
+            print("${this[i]},")
         } else {
-            ""
-        } + this[i].toString())
+            println("${this[i]}")
+        }
     }
 }
--- src/test/java/TestTenSort.kt
+++ src/test/java/TestTenSort.kt
@@ -5,4 +5,26 @@ class TestTenSort {
     fun testBubbleSort1() {
         TenSortAlgorithm.bubbleSort1(TenSortAlgorithm.testArr).print()
     }
+
+    @Test
+    fun testBubbleSort2() {
+        TenSortAlgorithm.bubbleSort2(TenSortAlgorithm.testArr).print()
+    }
+
+    @Test
+    fun testSelectionSort() {
+        TenSortAlgorithm.selectionSort(TenSortAlgorithm.testArr).print()
+    }
+
+    @Test
+    fun testInsertionSort() {
+        TenSortAlgorithm.insertionSort(TenSortAlgorithm.testArr).print()
+    }
+
+    @Test
+    fun testShellSort() {
+        TenSortAlgorithm.shellSort(TenSortAlgorithm.testArr).print()
+    }
+
+
 }
